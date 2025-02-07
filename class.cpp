717c4264e19a55c723148fe9685c114769b63727
dns0191 <qkdrldbs0191@naver.com>
2025-02-07 13:22:14 +0900

    복붙 완료

--- class.cpp
+++ class.cpp
@@ -1,153 +1,173 @@
 #include "class.h"
 #include "Function.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
 
 constexpr auto LEFT_SIDE = false;
 constexpr auto RIGHT_SIDE = true;
 
 void MultiGroupNode::makeOneDimensionalFlux(double* C[3][5], double* source_avg, double* surf_source[3][2])
 {
-    const int ng = number_of_groups;
-    int g;
-    for (g = 0; g < ng; g++)
-    {
-        SRC1[g] = 0.0;
-        SRC2[g] = 0.0;
-    }
+	const int ng = number_of_groups;
+	int g;
+	for (g = 0; g < ng; g++)
+	{
+		SRC1[g] = 0.0;
+		SRC2[g] = 0.0;
+	}
 
-    for (int u = 0; u < dim; u++)
-    {
-        for (g = 0; g < ng; g++)
-        {
-            const double flux_l = getSurfaceFlux(u, LEFT_SIDE, g);
-            const double flux_r = getSurfaceFlux(u, RIGHT_SIDE, g);
-            C[u][0][g] = flux_avg[g];
-            C[u][1][g] = (flux_r - flux_l) / 2.0;
-            C[u][2][g] = (flux_r + flux_l) / 2.0 - flux_avg[g];
-            SRC1[g] = DL[u][1][g];
-            SRC2[g] = DL[u][2][g];
-        }
-        add_product(SRC1, SRC1, M1[u], C[u][1], ng);
-        add_product(SRC2, SRC2, M2[u], C[u][2], ng);
-        GaussianElimination(M3[u], C[u][3], SRC1, ng);
-        GaussianElimination(M4[u], C[u][4], SRC2, ng);
-    }
+	for (int u = 0; u < dim; u++)
+	{
+		for (g = 0; g < ng; g++)
+		{
+			const double flux_l = getSurfaceFlux(u, LEFT_SIDE, g);
+			const double flux_r = getSurfaceFlux(u, RIGHT_SIDE, g);
+			C[u][0][g] = flux_avg[g];
+			C[u][1][g] = (flux_r - flux_l) / 2.0;
+			C[u][2][g] = (flux_r + flux_l) / 2.0 - flux_avg[g];
+			SRC1[g] = DL[u][1][g];
+			SRC2[g] = DL[u][2][g];
+		}
+		add_product(SRC1, SRC1, M1[u], C[u][1], ng);
+		add_product(SRC2, SRC2, M2[u], C[u][2], ng);
+		GaussianElimination(M3[u], C[u][3], SRC1, ng);
+		GaussianElimination(M4[u], C[u][4], SRC2, ng);
+	}
 }
 
 void MultiGroupNode::updateAverageFlux(double* C[3][5], const double* source_avg)
 {
-    const int ng = number_of_groups;
-    int g;
+	const int ng = number_of_groups;
+	int g;
 
-    for (int tg = 0; tg < ng; tg++)
-    {
-        for (g = 0; g < ng; g++)
-        {
-            MM[tg][g] = A[tg][g];
-            SRC[tg] = source_avg[tg];
-        }
-    }
+	for (int tg = 0; tg < ng; tg++)
+	{
+		for (g = 0; g < ng; g++)
+		{
+			MM[tg][g] = A[tg][g];
+			SRC[tg] = source_avg[tg];
+		}
+	}
 
-    for (int u = 0; u < dim; u++)
-    {
-        node_width[u] = getNodeWidth(u);
-        for (g = 0; g < ng; g++)
-        {
-            MM[g][g] += 12.0 * Q[u][0][g] / node_width[u];
-        }
-    }
+	for (int u = 0; u < dim; u++)
+	{
+		node_width[u] = getNodeWidth(u);
+		for (g = 0; g < ng; g++)
+		{
+			MM[g][g] += 12.0 * Q[u][0][g] / node_width[u];
+		}
+	}
 
-    for (g = 0; g < number_of_groups; g++)
-    {
-        for (int u = 0; u < dim; u++)
-        {
-            const double j_in_l = getIncomingCurrent(u, LEFT_SIDE, g);
-            const double j_in_r = getIncomingCurrent(u, RIGHT_SIDE, g);
-            const double Q4 = 1.0 - Q[u][2][g] - Q[u][3][g];
-            SRC[g] += (2.0 * Q[u][0][g] * C[u][4][g] + Q4 * (j_in_l + j_in_r)) / node_width[u];
-        }
-    }
-    GaussianElimination(MM, new_flux, SRC, ng);
+	for (g = 0; g < number_of_groups; g++)
+	{
+		for (int u = 0; u < dim; u++)
+		{
+			const double j_in_l = getIncomingCurrent(u, LEFT_SIDE, g);
+			const double j_in_r = getIncomingCurrent(u, RIGHT_SIDE, g);
+			const double Q4 = 1.0 - Q[u][2][g] - Q[u][3][g];
+			SRC[g] += (2.0 * Q[u][0][g] * C[u][4][g] + Q4 * (j_in_l + j_in_r)) / node_width[u];
+		}
+	}
+	GaussianElimination(MM, new_flux, SRC, ng);
 }
 
 void MultiGroupNode::updateOutgoingCurrent(double* C[3][5])
 {
-    for (int u = 0; u < dim; u++)
-    {
-        for (int g = 0; g < number_of_groups; g++)
-        {
-            const double j_in_l = getIncomingCurrent(u, LEFT_SIDE, g);
-            const double j_in_r = getIncomingCurrent(u, RIGHT_SIDE, g);
-            const double j_out_l = Q[u][0][g] * (6 * flux_avg[g] - C[u][4][g]) + Q[u][1][g] * C[u][3][g] + Q[u][2][g] * j_in_r + Q[u][3][g] * j_in_l;
-            const double j_out_r = Q[u][0][g] * (6 * flux_avg[g] - C[u][4][g]) + Q[u][1][g] * C[u][3][g] + Q[u][2][g] * j_in_l + Q[u][3][g] * j_in_r;
-        }
-    }
+	for (int u = 0; u < dim; u++)
+	{
+		for (int g = 0; g < number_of_groups; g++)
+		{
+			const double j_in_l = getIncomingCurrent(u, LEFT_SIDE, g);
+			const double j_in_r = getIncomingCurrent(u, RIGHT_SIDE, g);
+			const double j_out_l = Q[u][0][g] * (6 * flux_avg[g] - C[u][4][g]) + Q[u][1][g] * C[u][3][g] + Q[u][2][g] * j_in_r + Q[u][3][g] * j_in_l;
+			const double j_out_r = Q[u][0][g] * (6 * flux_avg[g] - C[u][4][g]) + Q[u][1][g] * C[u][3][g] + Q[u][2][g] * j_in_l + Q[u][3][g] * j_in_r;
+		}
+	}
 }
 
 void MultiGroupNode::updateTransverseLeakage(int u, int g)
 {
-    DL[u][0][g] = 0.0;
-    for (int i = 1; i < dim; i++)
-    {
-        const int v = (u + i) % dim;
-        DL[u][0][g] += (getSurfaceNetCurrent(v, RIGHT_SIDE, g) - getSurfaceNetCurrent(v, LEFT_SIDE, g)) / getNodeWidth(v);
-    }
+	DL[u][0][g] = 0.0;
+	for (int i = 1; i < dim; i++)
+	{
+		const int v = (u + i) % dim;
+		DL[u][0][g] += (getSurfaceNetCurrent(v, RIGHT_SIDE, g) - getSurfaceNetCurrent(v, LEFT_SIDE, g)) / getNodeWidth(v);
+	}
 
-	Node* l_node = getNeighborNode(u, LEFT_SIDE);
-	Node* r_node = getNeighborNode(u, RIGHT_SIDE);
+	l_node = getNeighborNode(u, LEFT_SIDE);
+	r_node = getNeighborNode(u, RIGHT_SIDE);
 
-    const double h_c = getNodeWidth(u);
-    const double beta_c = getBeta(u, g);
-    const double d_c = getDiffusionCoefficient(g);
-    const double DL0_c = DL[u][0][g];
-    const double h_l = l_node->getNodeWidth(u);
-    const double h_r = r_node->getNodeWidth(u);
-    const double beta_l = l_node->getBeta(u, g);
-    const double beta_r = r_node->getBeta(u, g);
-    const double DL0_l = l_node->getAverageTransverseLeakage(u, g);
-    const double DL0_r = r_node->getAverageTransverseLeakage(u, g);
-    const double L_l = (DL0_l / h_l + DL0_c / h_c) / (beta_l + beta_c);
-    const double L_r = (DL0_c / h_c + DL0_r / h_r) / (beta_c + beta_r);
+	const double h_c = getNodeWidth(u);
+	const double beta_c = getBeta(u, g);
+	const double d_c = getDiffusionCoefficient(g);
+	const double DL0_c = DL[u][0][g];
+	const double h_l = l_node->getNodeWidth(u);
+	const double h_r = r_node->getNodeWidth(u);
+	const double beta_l = l_node->getBeta(u, g);
+	const double beta_r = r_node->getBeta(u, g);
+	const double DL0_l = l_node->getAverageTransverseLeakage(u, g);
+	const double DL0_r = r_node->getAverageTransverseLeakage(u, g);
+	L_l = (DL0_l / h_l + DL0_c / h_c) / (beta_l + beta_c);
+	L_r = (DL0_c / h_c + DL0_r / h_r) / (beta_c + beta_r);
 
-    DL[u][1][g] = d_c * (L_r - L_l) / 2.0;
-    DL[u][2][g] = d_c * (L_r + L_l - 2.0 * DL0_c / d_c) / 2.0;
+	DL[u][1][g] = d_c * (L_r - L_l) / 2.0;
+	DL[u][2][g] = d_c * (L_r + L_l - 2.0 * DL0_c / d_c) / 2.0;
 }
 
-void MultiGroupNode::getDimension(double* matrix)
+void MultiGroupNode::getDimension()
 {
-
+	for (int i = 0; i < 3; i++)
+	{
+		if (getNodeWidth(i) != 0.0)
+		{
+			dim = i + 1;
+		}
+	}
 }
 
 double MultiGroupNode::getNodeWidth(int direction)
 {
-    return node_width[direction];
+	return node_width[direction];
 }
 
 double MultiGroupNode::getSurfaceFlux(int u, bool side, int number_of_group)
 {
-    return 0;
+	return 0;
 }
 
 double MultiGroupNode::getSurfaceNetCurrent(int direction, bool side, int number_of_group)
 {
-    return 0;
+	return 0;
 }
 
 double MultiGroupNode::getBeta(int direction, int number_of_group)
 {
-    return getDiffusionCoefficient(number_of_group) / getNodeWidth(direction);
+	return getDiffusionCoefficient(number_of_group) / getNodeWidth(direction);
 }
 
 double MultiGroupNode::getDiffusionCoefficient(int number_of_group)
 {
-    return D_c[number_of_group][number_of_group];
+	return D_c[number_of_group][number_of_group];
 }
 
 double MultiGroupNode::getIncomingCurrent(int direction, bool side, int number_of_group)
 {
-    return 0;
+	return 0;
 }
 
-Node* MultiGroupNode::getNeighborNode(int direction, bool side)
+double MultiGroupNode::getAverageTransverseLeakage(int direction, int group)
 {
-    return 0;
+	return 0;
+}
+
+MultiGroupNode* MultiGroupNode::getNeighborNode(int direction, bool side)
+{
+	if (side == 1)
+	{
+
+	}
+	return 0;
 }
--- class.h
+++ class.h
@@ -1,21 +1,14 @@
 #pragma once
 
-class Node {
-public:
-    double getNodeWidth(int direction);
-    double getBeta(int direction, int group);
-    double getAverageTransverseLeakage(int direction, int group);
-};
-
 class MultiGroupNode {
 private:
-    static int number_of_groups;
-    static int dim;
-    static double* DL[3][3], * node_width, * flux_avg, * old_flux, *new_flux, * out_current[3][2];
+    int number_of_groups;
+    int dim;
+    double* DL[3][3], * node_width, * flux_avg, * old_flux, *new_flux, * out_current[3][2];
     /*
 	DL: Transverse Leakage
     */
-    static double** A, ** M1[3], ** M2[3], ** M3[3], ** M4[3], ** D_c, **MM;
+    double** A, ** M1[3], ** M2[3], ** M3[3], ** M4[3], ** D_c, **MM;
     /*
 	A: Removal Cross Section
     M1: AC1 + DL1
@@ -25,28 +18,28 @@ private:
 	D_c: Diffusion Coefficient
     MM: Q0
     */
-    static double* Q[3][4];
-    static double* C0[3], * C1[3], * C2[3], * C3[3], * C4[3];
-    static double* B3[3][3], * B4[3][3];
-    static double* SRC, * SRC1, * SRC2;
+    double* Q[3][4];
+    double* C0[3], * C1[3], * C2[3], * C3[3], * C4[3];
+    double* B3[3][3], * B4[3][3];
+    double* SRC, * SRC1, * SRC2;
     //SRC: Node Average Flux -> s
-    static int neighbor_node[6];
-    static double mgxs;
+    int neighbor_node[6];
+    double mgxs, L_l, L_r;
+    MultiGroupNode* l_node, * r_node;
 
 public:
-    static void makeOneDimensionalFlux(double* C[3][5], double* source_avg, double* surf_source[3][2]);
-    static void updateAverageFlux(double* C[3][5], const double* source_avg);
-    static void updateOutgoingCurrent(double* C[3][5]);
-    static void updateTransverseLeakage(int u, int group);
-    static void getDimension(double* matrix);
-    static double getNodeWidth(int direction);
-    static double getSurfaceFlux(int u, bool side, int number_of_group);
-    static double getSurfaceNetCurrent(int direction, bool side, int number_of_group);
-    static double getBeta(int direction, int number_of_group);
-    static double getDiffusionCoefficient(int number_of_group);
-    static double getIncomingCurrent(int direction, bool side, int number_of_group);
-    static Node* getNeighborNode(int direction, bool side);
-    //static void Initializer(const std::string&filename);
-    //static double getDiffer(double* flux1, double* flux2);
+    void makeOneDimensionalFlux(double* C[3][5], double* source_avg, double* surf_source[3][2]);
+    void updateAverageFlux(double* C[3][5], const double* source_avg);
+    void updateOutgoingCurrent(double* C[3][5]);
+    void updateTransverseLeakage(int u, int group);
+    void getDimension();
+    double getNodeWidth(int direction);
+    double getSurfaceFlux(int u, bool side, int number_of_group);
+    double getSurfaceNetCurrent(int direction, bool side, int number_of_group);
+    double getBeta(int direction, int number_of_group);
+    double getDiffusionCoefficient(int number_of_group);
+    double getIncomingCurrent(int direction, bool side, int number_of_group);
+    double getAverageTransverseLeakage(int direction, int group);
+    MultiGroupNode* getNeighborNode(int direction, bool side);
 };
 
