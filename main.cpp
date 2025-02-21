c809bbff442a6e2478bf6110cfd8d63bab18bdbe
dns0191 <qkdrldbs0191@naver.com>
2025-02-21 10:29:15 +0900

    경고문 수정 완료

--- main.cpp
+++ main.cpp
@@ -12,15 +12,16 @@ int main() {
         return 1;
     }
     debugFile.close();
-    const double error = initializeNodesFromInput("input.inp");
-    std::cout << "nodeGrid1D size: " << nodeGrid1D.size() << std::endl;
-    std::cout << "nodeGrid2D size: " << nodeGrid2D.size() << ", " << nodeGrid2D[0].size() << std::endl;
+    const double error = initializeNodesFromInput("input.txt");
+    std::cout << "nodeGrid1D size: " << nodeGrid1D.size() << "\n";
+    std::cout << "nodeGrid2D size: " << nodeGrid2D.size() << ", " << nodeGrid2D[0].size() << "\n";
 
-    int step = 0;
+    
     debugPrintNodes();
     std::ofstream outputFile("output.out");
     if (outputFile.is_open()) {
         outputFile << "     ";
+        int step = 0;
         for (const auto& row : nodeGrid2D) {
             for (const auto& node : row) {
                 if (node != nullptr) {
